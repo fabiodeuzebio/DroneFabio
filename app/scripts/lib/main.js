ca44ab4a211d01bdb60027dcca8d868ad4008c56
fabiodeuzebio <fabiodeuzebio@gmail.com>
2015-03-24 17:52:24 -0300

    update main.js

--- app/scripts/lib/main.js
+++ app/scripts/lib/main.js
@@ -21,5 +21,130 @@ $(document).ready(function() {
 
     });
 
-    
+
 });
+
+(function() {
+
+    'use strict';
+
+    angular.module('DroneFabio', [])
+        .factory('DroneFabio.DroneService', DroneService)
+        .controller('DroneFabio.RadioControleCtrl', RadioControleCtrl)
+        .controller('DroneFabio.SensoresCtrl', SensoresCtrl)
+        .controller('DroneFabio.GpsCtrl', GpsCtrl);
+
+    function DroneService() {
+        var metodosPublicos = {
+            getGpsAltitude: _getGpsAltitude,
+            getGpsSatelites: _getGpsSatelites
+        };
+
+        var dataService = {
+            altitude: undefined,
+            rssi: undefined,
+            gpsAltitude: undefined,
+            gpsSat: undefined,
+            gpsLatLon: undefined,
+            gpsSpeed: undefined,
+            angx: undefined,
+            angy: undefined,
+            hdg: undefined
+        };
+
+        var socket = io.connect('');
+
+        socket.on('dataToUi', function(data) {
+            dataService.altitude = data.altitude;
+            dataService.rssi = data.rssi;
+            dataService.gpsAltitude = data.gpsAltitude;
+            dataService.gpsSat = data.gpsSat;
+            dataService.gpsLatLon = data.gpsLat + ' / ' + data.gpsLon;
+            dataService.gpsSpeed = data.gpsSpeed;
+            dataService.angx = data.angx;
+            dataService.angy = data.angy;
+            dataService.hdg = data.hdg;
+        });
+
+        function _getGpsAltitude() {
+            return dataService.altitude;
+        }
+
+        function _getGpsSatelites() {
+            return dataService.gpsSat;
+        }
+
+
+        return metodosPublicos;
+    }
+
+    /**
+     * Rádio Controle
+     * @type {Array}
+     */
+    RadioControleCtrl.$inject = ['$injector'];
+
+    function RadioControleCtrl($injector) {
+        var viewModel = this;
+        var DroneService = $injector.get('DroneFabio.DroneService');
+
+        init();
+
+        function init() {
+            initName();
+
+            function initName() {
+                getName();
+            }
+        }
+
+        function getName() {
+            viewModel.gpsAltitude = DroneService.getGpsAltitude();
+        }
+    }
+
+    /**
+     * Sensores
+     * @type {Array}
+     */
+    SensoresCtrl.$inject = ['$injector'];
+
+    function SensoresCtrl($injector) {
+        var viewModel = this;
+        var DroneService = $injector.get('DroneFabio.DroneService');
+
+        init();
+
+        function init() {
+            initName();
+
+            function initName() {
+                getName();
+            }
+        }
+
+        function getName() {
+            viewModel.gpsAltitude = DroneService.getGpsAltitude();
+        }
+
+    }
+
+    /**
+     * GPS
+     */
+    GpsCtrl.$inject = ['$injector'];
+
+    function GpsCtrl($injector) {
+        var viewModel = this;
+        var DroneService = $injector.get('DroneFabio.DroneService');
+
+        viewModel.getAltitude = function() {
+            return DroneService.getGpsAltitude();
+        }
+
+        viewModel.getGpsSatelites = function() {
+            return DroneService.getGpsSatelites();
+        }
+    }
+
+})();
